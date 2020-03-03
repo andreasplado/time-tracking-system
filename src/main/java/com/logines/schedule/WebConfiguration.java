d038fdac147513de58f7b001ba2024ac129a1f18
Andreas Plado <andreasplado@gmail.com>
2020-03-03 16:57:20 +0200

    Integrated db

--- src/main/java/com/logines/schedule/WebConfiguration.java
+++ src/main/java/com/logines/schedule/WebConfiguration.java
@@ -5,13 +5,19 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
 @Configuration
 public class WebConfiguration {
 
-    @Bean
+    @PersistenceUnit
+    EntityManagerFactory emf;
+    /*@Bean
     ServletRegistrationBean h2servletRegistration() {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
-    }
+    }*/
+
 }
