886aad2943b74cc7d12c018992c2c9cac36ce6ef
Andreas Plado <andreasplado@gmail.com>
2020-03-03 17:38:30 +0200

    Integrated db

--- src/main/java/com/logines/schedule/ScheduleApplication.java
+++ src/main/java/com/logines/schedule/ScheduleApplication.java
@@ -1,6 +1,5 @@
 package com.logines.schedule;
 
-import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,10 +11,5 @@ public class ScheduleApplication {
 
     public static void main( String[] args ){
         SpringApplication.run( ScheduleApplication.class, args );
-    }
-
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server h2Server() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 }
