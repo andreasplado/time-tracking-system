942a8728ee6be4e72ccf0cc12765f29e89a18d15
Andreas Plado <andreasplado@gmail.com>
2020-03-03 14:43:46 +0200

    Added gradle wrapper!

---
+++ src/main/java/com/logines/schedule/configuration/BarDbConfig.java
@@ -0,0 +1,7 @@
+package com.logines.schedule.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+public class BarDbConfig {
+}
