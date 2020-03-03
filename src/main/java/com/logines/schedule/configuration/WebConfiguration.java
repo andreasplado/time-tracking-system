0ae3d322975a92bb65af928b687f64a64081dd4b
Andreas Plado <andreasplado@gmail.com>
2020-03-03 17:36:37 +0200

    Integrated db

--- src/main/java/com/logines/schedule/configuration/WebConfiguration.java
+++ src/main/java/com/logines/schedule/configuration/WebConfiguration.java
@@ -1,13 +1,10 @@
-package com.logines.schedule;
+package com.logines.schedule.configuration;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
-
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
