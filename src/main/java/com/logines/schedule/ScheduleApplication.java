c93662d18b2a6dbcaa458a2d8ba709fe7d214e1e
Andreas Plado <andreasplado@gmail.com>
2020-03-07 16:07:35 +0200

    Refactoring

--- src/main/java/com/logines/schedule/ScheduleApplication.java
+++ src/main/java/com/logines/schedule/ScheduleApplication.java
@@ -2,8 +2,10 @@ package com.logines.schedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.logines.schedule"})
 public class ScheduleApplication {
 
     public static void main( String[] args ){
