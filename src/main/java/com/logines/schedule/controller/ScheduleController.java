d154a5e19db14165690c17e3bf01e724e244c03a
Andreas Plado <andreasplado@gmail.com>
2020-03-07 16:31:44 +0200

    Removed logging level

--- src/main/java/com/logines/schedule/controller/ScheduleController.java
+++ src/main/java/com/logines/schedule/controller/ScheduleController.java
@@ -9,10 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.util.List;
 
@@ -25,8 +23,8 @@ public class ScheduleController {
     private StudentService studentService;
     @Autowired
     private JobService jobService;
-    
-    @GetMapping("/")
+
+    @RequestMapping(value="/",method = RequestMethod.GET)
     public String welcome(Model model) {
         List<Class> classes = classService.getAllClasses();
 
