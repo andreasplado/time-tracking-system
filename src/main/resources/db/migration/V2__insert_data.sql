56e404bb974b19edef235cb6e58191c348aad4b6
Andreas Plado <andreasplado@gmail.com>
2020-03-08 04:24:34 +0200

    Refactoring

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -1,5 +1,9 @@
 DROP TABLE student_classes;
 DROP TABLE studentclass;
-ALTER TABLE Job DROP COLUMN starttime, endtime;
-ALTER TABLE Class DROP COLUMN starttime, endtime, timeminutes, teachername;
+ALTER TABLE Job DROP COLUMN starttime;
+ALTER TABLE Job DROP COLUMN endtime;
+ALTER TABLE Class DROP COLUMN starttime;
+ALTER TABLE Class DROP COLUMN endtime;
+ALTER TABLE Class DROP COLUMN timeminutes;
+ALTER TABLE Class DROP COLUMN teachername;
 DROP TABLE student_classes_with_clashes;
