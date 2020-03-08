2ba73c2fa97a17cca50aeed8bd25a5c5ce96d40d
Andreas Plado <andreasplado@gmail.com>
2020-03-08 17:20:33 +0200

    Adding authentication

--- src/main/java/com/logines/schedule/configuration/WebConfiguration.java
+++ src/main/java/com/logines/schedule/configuration/WebConfiguration.java
@@ -7,18 +7,41 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.boot.autoconfigure.jdbc.JdbcOperationsDependsOnPostProcessor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.jdbc.SchemaManagement;
 import org.springframework.boot.jdbc.SchemaManagementProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ConditionalOnProperty(prefix = "spring.flyway", name = "enabled", matchIfMissing = true)
-public class WebConfiguration {
+public class WebConfiguration  extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public DataSource ds() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+
 
     @Bean
     @Primary
@@ -59,5 +82,18 @@ public class WebConfiguration {
             super("flywayInitializer");
         }
 
+    }
+
+    @Bean
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.jdbcAuthentication().dataSource(ds())
+                .authoritiesByUsernameQuery("select USERNAME, ROLE from EMPLOYEE where USERNAME=?")
+                .usersByUsernameQuery("select USERNAME, PASSWORD, 1 as enabled  from EMPLOYEE where USERNAME=?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
