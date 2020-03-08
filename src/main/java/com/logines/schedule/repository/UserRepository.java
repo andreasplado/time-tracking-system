46b11a30dafc8b316566a8d502c9ec8cace739d1
Andreas Plado <andreasplado@gmail.com>
2020-03-08 17:42:17 +0200

    Adding authentication

--- src/main/java/com/logines/schedule/repository/UserRepository.java
+++ src/main/java/com/logines/schedule/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.logines.schedule.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query("SELECT s FROM users s WHERE s.name=:name")
