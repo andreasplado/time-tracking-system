4e9a4b9a02898588adfccf196c2abad36e27edac
Andreas Plado <andreasplado@gmail.com>
2020-03-09 19:50:02 +0200

    Refactoring

--- src/main/java/com/logines/schedule/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/logines/schedule/configuration/WebSecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,6 +26,7 @@ import javax.sql.DataSource;
 @Order(1)
 @ComponentScan
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
