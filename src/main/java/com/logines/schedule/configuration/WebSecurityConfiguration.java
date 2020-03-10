c7828f515829ce6b901aaab21dd73371c760c099
Andreas Plado <andreasplado@gmail.com>
2020-03-10 17:52:17 +0200

    Refactring

--- src/main/java/com/logines/schedule/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/logines/schedule/configuration/WebSecurityConfiguration.java
@@ -31,7 +31,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/resources/**", "/registration").permitAll()
+                .antMatchers("/resources/**", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
