f07c3d40d39e64995eddee6b3ce9dbad0a36e648
Andreas Plado <andreasplado@gmail.com>
2020-03-12 17:50:47 +0200

    Refatoring

--- src/main/java/com/logines/schedule/model/Users.java
+++ src/main/java/com/logines/schedule/model/Users.java
@@ -14,9 +14,6 @@ public class Users {
 
     private String password;
 
-    @Transient
-    private String passwordConfirm;
-
     @ManyToMany
     private Set<Role> roles;
 
@@ -42,14 +39,6 @@ public class Users {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getPasswordConfirm() {
-        return passwordConfirm;
-    }
-
-    public void setPasswordConfirm(String passwordConfirm) {
-        this.passwordConfirm = passwordConfirm;
     }
 
     public Set<Role> getRoles() {
