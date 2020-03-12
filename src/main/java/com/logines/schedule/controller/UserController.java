1d981ef7cf2f665c50e5af58d5a2e87eca0da1a0
Andreas Plado <andreasplado@gmail.com>
2020-03-12 21:00:51 +0200

    Adding model attribute

--- src/main/java/com/logines/schedule/controller/UserController.java
+++ src/main/java/com/logines/schedule/controller/UserController.java
@@ -56,7 +56,7 @@ public class UserController {
 
         userService.save(users);
 
-        securityService.autoLogin(users.getUsername(), model.getPasswordConfirm());
+        securityService.autoLogin(users.getUsername(), users.getPasswordConfirm());
 
         return "redirect:/";
     }
