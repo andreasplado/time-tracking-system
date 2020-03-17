9d795e10c16b888f728deb22e0f33c54fbef2abe
Andreas Plado <andreasplado@gmail.com>
2020-03-17 16:46:16 +0200

    Refactoring

--- src/main/java/com/logines/schedule/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/logines/schedule/configuration/WebSecurityConfiguration.java
@@ -55,6 +55,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
+    @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(bCryptPasswordEncoder());
     }
