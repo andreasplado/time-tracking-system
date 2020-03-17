e5a09f7211696da0e9e622d4cc4ecea14a32e049
Andreas Plado <andreasplado@gmail.com>
2020-03-17 17:01:12 +0200

    Refactoring

--- src/main/java/com/logines/schedule/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/logines/schedule/configuration/WebSecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -49,8 +50,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
 
-    @Bean
+    /*@Bean
     public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }*/
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
         return authenticationManager();
     }
 
