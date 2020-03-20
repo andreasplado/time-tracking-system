cac7d94c844d09d96a34b4a37649246016af355e
Andreas Plado <andreasplado@gmail.com>
2020-03-20 22:41:42 +0200

    Added resources

---
+++ src/main/java/com/logines/schedule/model/UserDetails.java
@@ -0,0 +1,74 @@
+package com.logines.schedule.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "UserDetails")
+public class UserDetails{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "fullname")
+    private String fullname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "idCode")
+    private String idCode;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "phone")
+    private String phone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIdCode() {
+        return idCode;
+    }
+
+    public void setIdCode(String idCode) {
+        this.idCode = idCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
--- src/main/resources/db/migration/V7__add_user_details.sql
+++ src/main/resources/db/migration/V7__add_user_details.sql
@@ -1,1 +1,12 @@
 ALTER TABLE Users ADD COLUMN id_code VARCHAR;
+
+CREATE TABLE UserDetails(
+  id INTEGER PRIMARY KEY,
+  fullname VARCHAR(255),
+  email VARCHAR(255),
+  idCode VARCHAR(255),
+  username VARCHAR(255),
+  phone VARCHAR(255),
+  startTime TIMESTAMP,
+  endTime TIMESTAMP
+);
