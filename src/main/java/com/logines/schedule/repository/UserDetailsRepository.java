3a718280df79cfc4f7fd934e516d6e39b2d6cc9d
Andreas Plado <andreasplado@gmail.com>
2020-03-20 23:32:18 +0200

    Added resources

--- src/main/java/com/logines/schedule/repository/UserDetailsRepository.java
+++ src/main/java/com/logines/schedule/repository/UserDetailsRepository.java
@@ -5,7 +5,9 @@ import com.logines.schedule.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Integer> {
 
     @Query("SELECT s FROM userDetails s WHERE s.username=:username")
