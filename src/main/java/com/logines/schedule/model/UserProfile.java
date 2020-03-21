b5130ec72e0dc11867d7a08c93d47f9977320ab2
Andreas Plado <andreasplado@gmail.com>
2020-03-21 19:39:03 +0200

    Refactoring

--- src/main/java/com/logines/schedule/model/UserProfile.java
+++ src/main/java/com/logines/schedule/model/UserProfile.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "UserProfile")
 public class UserProfile {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "fullname")
