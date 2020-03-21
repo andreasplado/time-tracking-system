7b8cbd2e8e90d7488d24046df78147ee57f2b8ef
Andreas Plado <andreasplado@gmail.com>
2020-03-21 02:48:09 +0200

    Refactoring

--- src/main/java/com/logines/schedule/repository/UserDetailsRepository.java
+++ src/main/java/com/logines/schedule/repository/UserDetailsRepository.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Integer> {
 
-    @Query("SELECT s FROM UserDetails s WHERE s.username=:username")
+    @Query("SELECT s FROM UserDetails s WHERE s.username=:username FETCH NEXT 1 ROWS ONLY")
     UserDetails findByUsername(@Param("username") String name);
 }
