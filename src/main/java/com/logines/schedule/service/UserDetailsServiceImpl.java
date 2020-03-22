1cbb653ec754d885dc1f1c23dfff05ef44259663
Andreas Plado <andreasplado@gmail.com>
2020-03-22 12:48:38 +0200

    Db

--- src/main/java/com/logines/schedule/service/UserDetailsServiceImpl.java
+++ src/main/java/com/logines/schedule/service/UserDetailsServiceImpl.java
@@ -26,9 +26,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         if (user == null) throw new UsernameNotFoundException(username);
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        for (Role role : user.getRoles()){
-            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
-        }
+        grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
 
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
     }
