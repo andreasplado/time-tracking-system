752af6718d5c5ba35bba343c9aee934ffed6025a
Andreas Plado <andreasplado@gmail.com>
2020-03-22 15:15:25 +0200

    Db

--- src/main/java/com/logines/schedule/model/WorkHour.java
+++ src/main/java/com/logines/schedule/model/WorkHour.java
@@ -79,4 +79,11 @@ public class WorkHour {
         this.title = title;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
