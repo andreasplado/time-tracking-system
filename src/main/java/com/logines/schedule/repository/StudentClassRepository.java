f805e29d910f0b8c547bca22919956e4d7a3179e
Andreas Plado <andreasplado@gmail.com>
2020-03-22 17:20:37 +0200

    Refactoring

--- src/main/java/com/logines/schedule/repository/StudentClassRepository.java
+++
@@ -1,6 +0,0 @@
-package com.logines.schedule.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentClassRepository extends JpaRepository<StudentClass, Integer> {
-}
