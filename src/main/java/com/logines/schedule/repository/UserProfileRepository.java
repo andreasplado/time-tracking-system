aa044896ccc3eae23f18d0aa5d5b4927b2fd3cdb
Andreas Plado <andreasplado@gmail.com>
2020-03-22 17:54:56 +0200

    Refactoring

--- src/main/java/com/logines/schedule/repository/UserProfileRepository.java
+++ src/main/java/com/logines/schedule/repository/UserProfileRepository.java
@@ -1,6 +1,5 @@
 package com.logines.schedule.repository;
 
-import com.logines.schedule.model.Student;
 import com.logines.schedule.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -8,8 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
