0aeaad74a92512a0c623de7ceddf750269ca6023
Andreas Plado <andreasplado@gmail.com>
2020-03-22 06:36:02 +0200

    Flyway

--- src/main/resources/db/migration/V3__insert_users.sql
+++ src/main/resources/db/migration/V3__insert_users.sql
@@ -18,7 +18,7 @@ CREATE TABLE UserProfile(
 CREATE TABLE WorkHour (
   id INTEGER PRIMARY KEY,
   title VARCHAR(255),
-  email  VARCHAR(255),
+  username  VARCHAR(255),
   end_time  VARCHAR(255),
   start_tme  VARCHAR(255),
   created_at  DATE NOT NULL DEFAULT CURRENT_DATE,
