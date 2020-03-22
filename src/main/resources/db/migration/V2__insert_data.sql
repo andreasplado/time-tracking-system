4bd3ab7ecedbacab1c0758c7932213e5043d3a52
Andreas Plado <andreasplado@gmail.com>
2020-03-22 05:49:35 +0200

    Refactoring

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -5,4 +5,3 @@ ALTER TABLE Class DROP COLUMN starttime;
 ALTER TABLE Class DROP COLUMN endtime;
 ALTER TABLE Class DROP COLUMN timeminutes;
 ALTER TABLE Class DROP COLUMN teachername;
-DROP TABLE student_classes_with_clashes;
