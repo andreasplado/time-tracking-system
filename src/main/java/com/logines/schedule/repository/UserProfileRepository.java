26bbb4257d72774dc7bc8c0dd4793b11a7350f7b
Andreas Plado <andreasplado@gmail.com>
2020-03-22 12:40:00 +0200

    Db

--- src/main/java/com/logines/schedule/repository/UserProfileRepository.java
+++ src/main/java/com/logines/schedule/repository/UserProfileRepository.java
@@ -3,15 +3,19 @@ package com.logines.schedule.repository;
 import com.logines.schedule.model.Student;
 import com.logines.schedule.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
 
+    @Transactional
+    @Modifying
     @Query("DELETE FROM UserProfile WHERE username=:username")
     void deleteByFirstName(@Param("username") String username);
 }
