95e358f4ca43357e68f76146f48fa8836e2f0602
Andreas Plado <andreasplado@gmail.com>
2020-03-31 02:56:01 +0300

    Refactoring time

--- src/main/java/com/logines/schedule/utils/DateUtils.java
+++ src/main/java/com/logines/schedule/utils/DateUtils.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class DateUtils {
 
     public static String dateToString(Date date){
-        Format formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        Format formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm");
         return formatter.format(date);
     }
 }
