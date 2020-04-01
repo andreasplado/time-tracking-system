8d00e713666c6928a8f51dfc35a7b296766e9f15
Andreas Plado <andreasplado@gmail.com>
2020-04-01 20:56:06 +0300

    Refactoring

--- src/main/java/com/logines/schedule/repository/WorkHourRepository.java
+++ src/main/java/com/logines/schedule/repository/WorkHourRepository.java
@@ -15,6 +15,6 @@ public interface WorkHourRepository extends JpaRepository<WorkHour, Integer> {
     @Query("SELECT s FROM WorkHour s WHERE s.username=:username")
     List<WorkHour> findWorkHoursByUsername(@Param("username") String name);
 
-    @Query(value = "DELETE FROM logines.work_hour WHERE to_date(cast(created_at as TEXT),'MM-DD-YYYY HH:mm') < NOW() - INTERVAL '30 days'", nativeQuery = true)
+    @Query(value = "DELETE FROM logines.work_hour WHERE to_date(cast(created_at as TEXT),'MM-DD-YYYY HH:mi') < NOW() - INTERVAL '30 days'", nativeQuery = true)
     List<WorkHour> deleteLast30DaysWorkHours();
 }
