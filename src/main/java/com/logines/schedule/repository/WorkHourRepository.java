7e673ea7105a7a3dbd4b7a8d688ea35006fb27e9
Andreas Plado <andreasplado@gmail.com>
2020-04-01 22:22:44 +0300

    Refactoring

--- src/main/java/com/logines/schedule/repository/WorkHourRepository.java
+++ src/main/java/com/logines/schedule/repository/WorkHourRepository.java
@@ -3,6 +3,7 @@ package com.logines.schedule.repository;
 import com.logines.schedule.model.Users;
 import com.logines.schedule.model.WorkHour;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,6 +16,7 @@ public interface WorkHourRepository extends JpaRepository<WorkHour, Integer> {
     @Query("SELECT s FROM WorkHour s WHERE s.username=:username")
     List<WorkHour> findWorkHoursByUsername(@Param("username") String name);
 
+    @Modifying
     @Query(value = "DELETE FROM logines.work_hour WHERE to_date(cast(created_at as TEXT),'MM-DD-YYYY HH24:mi') < NOW() - INTERVAL '30 days'", nativeQuery = true)
     List<WorkHour> deleteLast30DaysWorkHours();
 }
