5d0b2e38537959fadc3715138e547c582d38802d
Andreas Plado <andreasplado@gmail.com>
2020-04-01 19:54:07 +0300

    Register update

--- src/main/java/com/logines/schedule/cronjob/HelloJob.java
+++ src/main/java/com/logines/schedule/cronjob/HelloJob.java
@@ -3,7 +3,9 @@ package com.logines.schedule.cronjob;
 import com.logines.schedule.service.WorkHourService;
 import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class HelloJob implements Job {
 
     @Autowired
