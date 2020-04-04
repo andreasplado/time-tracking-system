e848535116d7eaac8fc1ff58f88022a482edb9ef
Andreas Plado <andreasplado@gmail.com>
2020-04-04 07:17:31 +0300

    Timestamp

--- src/main/java/com/logines/schedule/model/WorkHour.java
+++ src/main/java/com/logines/schedule/model/WorkHour.java
@@ -27,10 +27,10 @@ public class WorkHour {
     private String username;
 
     @Column(name = "start_time")
-    private Instant start_time;
+    private String start_time;
 
     @Column(name = "end_time")
-    private Instant end_time;
+    private String end_time;
 
 
     @Column(name = "lunch_time")
@@ -82,19 +82,19 @@ public class WorkHour {
         this.username = username;
     }
 
-    public void setStart_time(Instant start_time) {
+    public void setStart_time(String start_time) {
         this.start_time = start_time;
     }
 
-    public Instant getStart_time(){
+    public String getStart_time(){
         return this.start_time;
     }
 
-    public void setEnd_time(Instant end_time) {
+    public void setEnd_time(String end_time) {
         this.end_time = end_time;
     }
 
-    public Instant getEnd_time(){
+    public String getEnd_time(){
         return this.end_time;
     }
 
