eb1deb2ab66945b2d47b235d5ceecbb1a334bf15
Andreas Plado <andreasplado@gmail.com>
2020-04-04 13:44:22 +0300

    Refact

--- src/main/java/com/logines/schedule/service/WorkHourService.java
+++ src/main/java/com/logines/schedule/service/WorkHourService.java
@@ -90,7 +90,7 @@ public class WorkHourService {
             System.out.println("Between start: "  + Duration.between(startDateTime, endDateTime).getSeconds());
             long nanoseconds = lunchTime.getSecond();
             diff -= nanoseconds;
-            System.out.println("lunch: "  + lunchTime.getNano());
+            System.out.println("lunch: "  + lunchTime.getSecond());
         }
 
 
