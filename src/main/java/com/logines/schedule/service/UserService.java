e01ec5dca3fcdf0ab48a7855406b83d0e705a382
Andreas Plado <andreasplado@gmail.com>
2020-04-06 13:51:08 +0300

    User edit

--- src/main/java/com/logines/schedule/service/UserService.java
+++ src/main/java/com/logines/schedule/service/UserService.java
@@ -2,9 +2,11 @@ package com.logines.schedule.service;
 
 import com.logines.schedule.model.Users;
 import org.h2.engine.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
     void save(Users user);
 
--- src/main/java/com/logines/schedule/service/UserServiceImpl.java
+++ src/main/java/com/logines/schedule/service/UserServiceImpl.java
@@ -5,10 +5,12 @@ import com.logines.schedule.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
 
