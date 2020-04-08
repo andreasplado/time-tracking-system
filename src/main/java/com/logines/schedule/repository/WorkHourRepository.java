105451c4ed81194268c9303018f183d9012562e9
Andreas Plado <andreasplado@gmail.com>
2020-04-08 21:07:46 +0300

    Query

--- src/main/java/com/logines/schedule/repository/WorkHourRepository.java
+++ src/main/java/com/logines/schedule/repository/WorkHourRepository.java
@@ -30,10 +30,10 @@ public interface WorkHourRepository extends JpaRepository<WorkHour, Integer> {
     //@Query(value = "SELECT s FROM logines.work_hour s WHERE TO_TIMESTAMP(s.start_time,  'YYYY-MM-DD HH24:mi')\\:\\:timestamp BETWEEN :start_time\\:\\:timestamp AND now()\\:\\:timestamp", nativeQuery = true)
     //@Query(value = "SELECT s.* FROM logines.work_hour s WHERE TO_TIMESTAMP(s.start_time,  'YYYY-MM-DD HH24:mi') BETWEEN :start_time\\:\\:timestamp AND now()\\:\\:timestamp", nativeQuery = true)
     //@Query(value = "SELECT s.* FROM logines.work_hour s WHERE TO_TIMESTAMP(s.start_time,  'YYYY-MM-DD HH24:mi')\\:\\:timestamp BETWEEN :start_time\\:\\:timestamp AND now()\\:\\:timestamp", nativeQuery = true)
-    @Query(value = "SELECT s FROM 'logines.'work_hour' WHERE s.start_time between '2020-04-06 23:03' and '2020-04-09 23:03'", nativeQuery = true)
+    @Query(value = "SELECT s FROM 'logines'.'work_hour' WHERE s.start_time between '2020-04-06 23:03' and '2020-04-09 23:03'", nativeQuery = true)
     List<WorkHour> findByStartTime(@Param("start_time") String startTime);
 
-    @Query(value ="SELECT s.* FROM WorkHour s WHERE s.end_time >= :end_time AND s.end_time < :end_time")
+    //@Query(value ="SELECT s FROM logines.\"work_hour\" s WHERE s.end_time >= :end_time AND s.end_time < :end_time", nativeQuery = true)
     List<WorkHour> findByEndTime(@Param("end_time") String endTime);
 
     //@Query(value = "DELETE FROM logines.work_hour WHERE to_date(cast(created_at as TEXT),'dd-MM-yyyy HH24:mi') < NOW() - INTERVAL '30 days'", nativeQuery = true)
