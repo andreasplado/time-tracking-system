3438d186ab0f07ffd4d5e496ce2a784235e42b5a
Andreas Plado <andreasplado@gmail.com>
2020-04-09 21:40:39 +0300

    Find between

--- src/main/java/com/logines/schedule/repository/WorkHourRepository.java
+++ src/main/java/com/logines/schedule/repository/WorkHourRepository.java
@@ -28,7 +28,7 @@ public interface WorkHourRepository extends JpaRepository<WorkHour, Integer> {
     //List<WorkHour> <findByStartTimeAndUsername>(@Param("start_time") String start_time, String username);
 
 
-    @Query(value = "SELECT s.* FROM logines.work_hour s WHERE s.start_time between ? and CURRENT_TIMESTAMP", nativeQuery = true)
+    @Query(value = "SELECT s.* FROM logines.work_hour s WHERE s.start_time between ? and now()::timestamp", nativeQuery = true)
     List<WorkHour> findByStartTime(String startTime);
 
     @Query(value = "SELECT s.* FROM logines.work_hour s WHERE s.start_time between ? and ?", nativeQuery = true)
