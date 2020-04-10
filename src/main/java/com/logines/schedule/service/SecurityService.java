16af747c6f778246f1a7d0ab8e7ab7e7f42bb83d
Andreas Plado <andreasplado@gmail.com>
2020-04-10 04:32:48 +0300

    Refactoring

--- src/main/java/com/logines/schedule/service/SecurityService.java
+++ src/main/java/com/logines/schedule/service/SecurityService.java
@@ -3,5 +3,4 @@ package com.logines.schedule.service;
 public interface SecurityService {
     String findLoggedInUsername();
     void autoLogin(String username, String password);
-    void logout();
 }
