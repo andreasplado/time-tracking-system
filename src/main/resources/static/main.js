7cfd3ed4ac49844d5c7330dc6b680d76959d7ab9
Andreas Plado <andreasplado@gmail.com>
2020-04-13 22:33:51 +0300

    Refactoring

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -9,33 +9,6 @@ $(document).ready(function(){
             $("#notes").val("No notes");
         }
     });
-        var availableTags = [
-          "ActionScript",
-          "AppleScript",
-          "Asp",
-          "BASIC",
-          "C",
-          "C++",
-          "Clojure",
-          "COBOL",
-          "ColdFusion",
-          "Erlang",
-          "Fortran",
-          "Groovy",
-          "Haskell",
-          "Java",
-          "JavaScript",
-          "Lisp",
-          "Perl",
-          "PHP",
-          "Python",
-          "Ruby",
-          "Scala",
-          "Scheme"
-        ];
-        $( "#user-search-by-username" ).autocomplete({
-          source: availableTags
-        });
 
 });
 
