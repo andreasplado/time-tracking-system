c2b03c7d8381a9c17be41059ef8a8368137ffb54
Andreas Plado <andreasplado@gmail.com>
2020-04-13 19:45:18 +0300

    Wourkhour calculator

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -12,9 +12,4 @@ $(document).ready(function(){
 
 });
 
-window.fitText( document.getElementById("tab-text1") );
-window.fitText( document.getElementById("tab-text2") );
-window.fitText( document.getElementById("tab-text3") );
-window.fitText( document.getElementById("tab-text4") );
-
 
