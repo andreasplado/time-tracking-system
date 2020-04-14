1d14ff0ec309ad75c72ca22082e73c7e1d1cbe33
Andreas Plado <andreasplado@gmail.com>
2020-04-14 18:51:35 +0300

    Refactoring

--- src/main/java/com/logines/schedule/repository/CompanyRepository.java
+++ src/main/java/com/logines/schedule/repository/CompanyRepository.java
@@ -13,6 +13,6 @@ import java.util.List;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
-    @Query("SELECT s FROM Company s WHERE s.fullname=:companyName")
+    @Query("SELECT s FROM Company s WHERE s.company_name=:companyName")
     List<Company> findByCompanyName(@Param("companyName") String companyName);
 }
