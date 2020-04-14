177b6266d5b6efef89a37e07e363ff0aecaab0ab
Andreas Plado <andreasplado@gmail.com>
2020-04-15 01:09:30 +0300

    Refactoring

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -12,4 +12,16 @@ $(document).ready(function(){
 
 });
 
-
+moment.locale('fr');
+console.log(moment(1316116057189).fromNow());
+console.log(moment.locales());
+const picker = new MaterialDatePicker().
+on('submit', val => {
+  events.innerHTML += val.toDate() + '\n';
+}).
+on('open', () => {
+  events.innerHTML += 'open\n';
+}).
+on('close', () => {
+  events.innerHTML += 'close\n';
+});
