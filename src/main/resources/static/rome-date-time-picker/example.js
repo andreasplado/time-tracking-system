0098c9ca3724f0b7dd33f1e37a70025f198d5a09
Andreas Plado <andreasplado@gmail.com>
2020-04-15 03:15:56 +0300

    Refactoring

--- src/main/resources/static/rome-date-time-picker/example.js
+++ src/main/resources/static/rome-date-time-picker/example.js
@@ -1,10 +1,10 @@
 var moment = rome.moment;
 
 rome(start_time, {dateValidator: rome.val.beforeEq(end_time),
-inputFormat: "YYYY-MM-DD HH:mm:ss"});
+inputFormat: "YYYY-MM-DD HH:mm:ss", autoHideOnClick: false});
     //dateValidator: rome.val.beforeEq(end_time)});
 rome(end_time, {dateValidator: rome.val.afterEq(start_time),
-inputFormat: "YYYY-MM-DD HH:mm:ss"});
+inputFormat: "YYYY-MM-DD HH:mm:ss", autoHideOnClick: false});
 rome(lunch_time, {inputFormat: "HH:mm:ss", date: false});
 rome(start_time_range, {dateValidator: rome.val.beforeEq(end_time_range), inputFormat: "YYYY-MM-DD HH:mm:ss"} );
 rome(end_time_range, {dateValidator: rome.val.afterEq(start_time_range), inputFormat: "YYYY-MM-DD HH:mm:ss"} );
