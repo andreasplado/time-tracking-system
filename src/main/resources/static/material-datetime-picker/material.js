a651075e7860820d7d25564f3aea66520b0fead9
Andreas Plado <andreasplado@gmail.com>
2020-04-19 00:49:14 +0300

    Material startTime

--- src/main/resources/static/material-datetime-picker/material.js
+++ src/main/resources/static/material-datetime-picker/material.js
@@ -1,15 +1,3 @@
-var start_time_range_picker = new MaterialDatetimePicker({
-    el: document.querySelector('#start_time_range'),
-    format: 'YYYY-MM-DD HH:mm:ss',
-    default: moment()
-});
-
-var end_time_range_picker = new MaterialDatetimePicker({
-    el: document.querySelector('#end_time_range'),
-    format: 'YYYY-MM-DD HH:mm:ss',
-    default: moment()
- });
-
 var start_time_picker = new MaterialDatetimePicker({
     el: document.querySelector('#start_time'),
     format: 'YYYY-MM-DD HH:mm:ss',
@@ -22,9 +10,6 @@ var end_time_picker = new MaterialDatetimePicker({
     default: moment()
 });
 
-var end_time_range = document.querySelector('#end_time_range');
-var start_time_range = document.querySelector('#start_time_range');
-var lunch_time = document.querySelector('#lunch_time');
 var start_time = document.querySelector('#start_time');
 var end_time = document.querySelector('#end_time');
 
@@ -37,13 +22,4 @@ start_time.addEventListener('click', function() {
 end_time.addEventListener('click', function() {
   end_time_picker.open();
   console.log("end time selected");
-}, false);
-
-
-end_time_range.addEventListener('click', function() {
-  end_time_range_picker.open();
-}, false);
-
-start_time_range.addEventListener('click', function() {
-  start_time_range_picker.open();
 }, false);
