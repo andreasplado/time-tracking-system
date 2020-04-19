7ae82fe7d066e2d04db4c24070558b2872c2f510
Andreas Plado <andreasplado@gmail.com>
2020-04-19 18:34:36 +0300

    Refactoring

--- src/main/java/com/logines/schedule/service/WorkHourService.java
+++ src/main/java/com/logines/schedule/service/WorkHourService.java
@@ -71,8 +71,11 @@ public class WorkHourService {
         //String sql = "SELECT extract(start_time from logines.work_hour) as hour_of_day FROM logines.work_hour WHERE username = ?";
         List<WorkHour> workHours = workHourRepository.findWorkHoursByUsername(username);
         System.out.println(Arrays.toString(workHours.toArray()));
+        long diff = 0;
         Timestamp startDateTime;
         Timestamp endDateTime;
+        LocalTime lunchTime;
+        Duration totalDuration = Duration.ZERO;
         long milliseconds = 0;
         for (int i = 0; i < workHours.size(); i++) {
 
@@ -80,19 +83,20 @@ public class WorkHourService {
 
             startDateTime = workHours.get(i).getStart_time(); //2020-04-19 10:00:00.0
             endDateTime = workHours.get(i).getEnd_time(); //2020-04-19 18:00:00.0
+            Duration duration = Duration.between(startDateTime.toLocalDateTime(), endDateTime.toLocalDateTime()); //00:30:00
+            totalDuration = duration.plusMinutes(duration.toMinutes());
+
+            //lunchTime = workHours.get(i).getLunch_time().toLocalTime(); //00:30:00
+
 
             // create a second time stamp
-            SimpleDateFormat format = new SimpleDateFormat("HH:mm");
-            Date d = null;
-            long difference = getDateDiff(startDateTime, endDateTime, TimeUnit.MILLISECONDS);
-            milliseconds += difference;
         }
-        if (workHours.size() != 0) {
-            int seconds = (int) milliseconds / 1000;
 
-            int hours = (int) milliseconds / 3600;
+        if(workHours.size()!= 0) {
+            int seconds = (int) totalDuration.getSeconds();
+
+            int hours = seconds / 3600;
             int minutes = (seconds % 3600) / 60;
-            seconds = (seconds % 3600) % 60;
 
             return hours + ":" + minutes;
         }
@@ -109,7 +113,7 @@ public class WorkHourService {
             Duration lunchTime = Duration.between(LocalTime.MIDNIGHT, workHour.getLunch_time().toLocalTime()); //00:30:00
             totalDuration = totalDuration.plusMinutes(lunchTime.toMinutes());
         }
-        if (workHours.size() != 0) {
+        if(workHours.size()!= 0) {
             return totalDuration.toMinutes();
         }
         return 0L;
