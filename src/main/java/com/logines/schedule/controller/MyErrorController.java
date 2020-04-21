0d06ed1d4c42f8f8d01ac1ad33c6bc6001cebc98
Andreas Plado <andreasplado@gmail.com>
2020-04-21 22:43:05 +0300

    Refactoring

--- src/main/java/com/logines/schedule/controller/MyErrorController.java
+++ src/main/java/com/logines/schedule/controller/MyErrorController.java
@@ -1,8 +1,10 @@
 package com.logines.schedule.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
@@ -12,18 +14,26 @@ import javax.servlet.http.HttpServletRequest;
 public class MyErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error_404";
+                model.addAttribute("error_title", "Oops! Page not found.");
+                model.addAttribute("error_content", "Sorry this page was not found");
+                model.addAttribute("error_nr", "<span>5</span><span>0</span><span>0</span>");
+
+                return "server_error";
             }
             else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error_500";
+                model.addAttribute("error_title", "Oops! Page error.");
+                model.addAttribute("error_content", "We are sorry, but the page have some technical issues");
+                model.addAttribute("error_nr", "<span>4</span><span>0</span><span>4</span>");
+                return "server_error";
             }
+            model.addAttribute("error", status);
         }
         return "error_page";
     }
