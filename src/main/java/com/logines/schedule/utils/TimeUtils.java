4674ca9f6b66d5e4fb01ef333568228c4a2363ec
Andreas Plado <andreasplado@gmail.com>
2020-04-24 13:55:02 +0300

    Refactoring

--- src/main/java/com/logines/schedule/utils/TimeUtils.java
+++ src/main/java/com/logines/schedule/utils/TimeUtils.java
@@ -17,14 +17,14 @@ public class TimeUtils {
 
     public static String timeUnitToFullTime(long time, TimeUnit timeUnit) {
         long day = timeUnit.toDays(time);
-        long hour = timeUnit.toHours(time) % 24;
+        long hour = timeUnit.toHours(time);
         long minute = timeUnit.toMinutes(time) % 60;
         long second = timeUnit.toSeconds(time) % 60;
 
 
-        if (day > 0) {
+        /*if (day > 0) {
             return String.format("%d day %02d:%02d:%02d", day, hour, minute, second);
-        } else if (hour > 0) {
+        } else */if (hour > 0) {
             return String.format("%d:%02d:%02d", hour, minute, second);
         } else if (minute > 0) {
             return String.format("%d:%02d", minute, second);
