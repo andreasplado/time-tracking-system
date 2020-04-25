bf3d324c3b696b1096efa3ff8031fa2511dd8f3c
Andreas Plado <andreasplado@gmail.com>
2020-04-25 11:05:58 +0300

    Refactoring

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -9,14 +9,26 @@ $(document).ready(function(){
             $("#notes").val("No notes");
         }
     });
+    var index = 'qpsstats-active-tab';
+    //  Define friendly data store name
+    var dataStore = window.sessionStorage;
+    var oldIndex = 0;
+    //  Start magic!
+    try {
+        // getter: Fetch previous value
+        oldIndex = dataStore.getItem(index);
+    } catch(e) {}
 
-    console.log($('a[data-toggle="tab"]'));
-	$( '.cd-tabs__item' ).on( 'click', function(){
-		window.location.hash = $(this).attr( 'id' );
-	});
-	// Check if we need to activate a tab based on the has
-	if ( window.location.hash && $( window.location.hash ).length ) {
-		$( window.location.hash ).click();
-	}
+    $( "#tabs" ).tabs({
+        active: oldIndex,
+        activate: function(event, ui) {
+            //  Get future value
+            var newIndex = ui.newTab.parent().children().index(ui.newTab);
+            //  Set future value
+            try {
+                dataStore.setItem( index, newIndex );
+            } catch(e) {}
+        }
+    });
 
 });
