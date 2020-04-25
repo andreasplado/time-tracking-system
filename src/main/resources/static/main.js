dffb87b667c82f86beb1ad5424055a7134d35e67
Andreas Plado <andreasplado@gmail.com>
2020-04-25 11:21:31 +0300

    Refactoring

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -12,28 +12,5 @@ $(document).ready(function(){
 
 
     //TABS
-    var $tabs = $('.cd-tabs__list'),
-        $tabsA = $tabs.find('a'),
-        $tabsC = $('.tab_content'),
-        start = window.location.hash || '#welcome';
-
-    function deactivate() {
-        $tabsA.removeClass('cd-tabs__item--selected');
-        $tabsC.hide();
-    }
-    function activate(href) {
-        $tabsA.filter('[href=' + href + ']').addClass('cd-tabs__item--selected');
-        $(href).fadeIn();
-    }
-
-    function clicked(e) {
-        var href = $(e.target).attr('href');
-        deactivate();
-        activate(href);
-    }
-
-    deactivate();
-    activate(start);
-    $tabs.on('click', 'a', clicked);
 
 });
