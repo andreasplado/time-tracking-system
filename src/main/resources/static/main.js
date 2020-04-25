139bef3ef02063dca33d111a7604f1ca66af59be
Andreas Plado <andreasplado@gmail.com>
2020-04-25 19:01:16 +0300

    Refactoring

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -13,9 +13,30 @@ $(document).ready(function(){
     var tab = [];
     for(var i = 1; i<6; i++){
         tab[i] = document.getElementById("tab-" + i);
+
         $("#tab-" + i).click(function() {
           alert( "Handler for .click() called." );
+          // Store
+          localStorage.setItem("tab", "tab-" + i);
         });
+    }
+
+    function defaultTab(){
+        var defaultTabName = document.getElementById("tab-1");
+        defaultTabName.className = "cd-tabs__item cd-tabs__item--selected";
+    }
+
+    function getTab(){
+        return localStorage.getItem("tab");
+    }
+
+    function activeTab(){
+        if(getTab() == null|| getTab() == "undefined"){
+            defaultTab();
+        }else{
+            var activatedTabName = document.getElementById(getTab());
+            activatedTabName.classList.add("cd-tabs__item--selected");
+        }
     }
 
 
